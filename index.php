488816cc256301b7ca13a7b1e6570de4fac9d14c
YukiNagano <ny712314@gmail.com>
2018-11-12 07:44:59 +0900

    first commit - index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Yuki Nagano</title>
+	<meta name="author" content="Yuki Nagano">
+	<meta name="description" content="This page is the official site of Yuki Nagano">
+</head>
+<body>
+</body>
+</html>
